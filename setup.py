12b168eafbaa2e4ee0ad75f19968178eed1d1b88
BrandesDenis <brandes654782@gmail.com>
2019-08-24 16:57:12 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="telegramreminder",
-    version="0.0.1",
+    version="0.0.2",
     description="telegram reminder bot",
     long_description=long_description,
     long_description_content_type="text/markdown",
